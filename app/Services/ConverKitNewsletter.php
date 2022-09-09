d328f9115043341a502ed357d466256bd3fc17d0
012kirby210 <012kirby210@gmail.com>
2022-09-09 18:34:08 +0200

    Add another newsletter service

---
+++ app/Services/ConverKitNewsletter.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Services;
+
+class ConverKitNewsletter implements Newsletter
+{
+    public function subscribe(string $email, string $list){
+        // specific convert kit implementation
+    }
+}
