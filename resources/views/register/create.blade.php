a69c06ab13c9af710340598f15a080424f1bc7c8
012kirby210 <012kirby210@gmail.com>
2022-09-11 17:16:18 +0200

    Refactor the register view with the form components

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -6,83 +6,15 @@
             </h1>
             <form method="POST" action="/register" class="mt-10">
                 @csrf
+                <x-form.input name="name" />
+                <x-form.input name="username" />
+                <x-form.input name="email" type="email" autocomplete="username"/>
+                <x-form.input name="password" type="password" autocomplete="new-password"/>
 
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                           for="name">
-                        Name
-                    </label>
-                    <input class="border border-grays-400 p-2 w-full"
-                           type="text"
-                           name="name"
-                           id="name"
-                           required
-                           value="{{ old('name') }}"
-                    >
-                    @error('name')
-                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                        for="username">
-                        Username
-                    </label>
-                    <input class="border border-grays-400 p-2 w-full"
-                           type="text"
-                           name="username"
-                           id="username"
-                           required
-                           value="{{old('username')}}"
-                    >
-                    @error('username')
-                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                           for="email">
-                        Email
-                    </label>
-                    <input class="border border-grays-400 p-2 w-full"
-                           type="email"
-                           name="email"
-                           id="email"
-                           required
-                           value="{{old('email')}}"
-                    >
-                    @error('email')
-                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                           for="password">
-                        Password
-                    </label>
-                    <input class="border border-grays-400 p-2 w-full"
-                           type="password"
-                           name="password"
-                           id="password"
-                           required
-                    >
-                    @error('password')
-                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-                <div class="mb-6">
-                    <button type="submit"
-                        class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">
-                        Submit
-                    </button>
-                </div>
-                @if ($errors->any())
-                    <ul>
-                    @foreach ($errors->all() as $error)
-                        <li class="text-red-500 text-xs">{{ $error }}</li>
-                    @endforeach
-                    </ul>
-                @endif
+                <x-form.button >
+                    Register
+                </x-form.button>
+
             </form>
         </div>
     </main>
