5d7f3c0ad61b34dc28a588e6f70a11d7ae5217ae
012kirby210 <012kirby210@gmail.com>
2022-09-11 15:27:18 +0200

    Use the form.button into the "add comment" section

--- resources/views/Posts/_add-comment-form.blade.php
+++ resources/views/Posts/_add-comment-form.blade.php
@@ -11,21 +11,19 @@
                 >
                 <h2>Want to participate</h2>
             </header>
-            <div class="mt-6">
-                            <textarea placeholder="Thing of something to say !"
-                                      name="body"
-                                      class="w-full text-sm focus:outline-none focus:ring"
-                                      cols="30"
-                                      rows="5"
-                                      required></textarea>
-                @error('body')
-                <span class="text-xs text-red-500">{{ $message }}</span>
-                @enderror
-            </div>
+            <x-form.field>
+                <textarea placeholder="Thing of something to say !"
+                          name="body"
+                          class="w-full text-sm focus:outline-none focus:ring"
+                          cols="30"
+                          rows="5"
+                          required></textarea>
+                <x-form.error name="body" />
+            </x-form.field>
             <div class="flex justify-end mt-6 border-t border-gray-200 pt-6">
-                <x-submit-button>
+                <x-form.button>
                     Post
-                </x-submit-button>
+                </x-form.button>
             </div>
         </form>
     </x-panel>
