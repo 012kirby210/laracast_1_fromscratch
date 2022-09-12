b884527435ff83b18f9ba23369ec0b137c6d18bc
012kirby210 <012kirby210@gmail.com>
2022-09-12 20:55:06 +0200

    Ad the post index admin page to the settings page

--- resources/views/components/settings.blade.php
+++ resources/views/components/settings.blade.php
@@ -1,10 +1,10 @@
 @props(['heading'])
-<section class="px-6 py-8 max-w-4xl mx-auto">
+<section class="px-6 py-8 max-w-6xl mx-auto">
     <h1 class="text-lg font-bold mb-8 pb-2 border-b">
         {{ ucfirst($heading) }}
     </h1>
     <div class="flex">
-        <aside class="w-48">
+        <aside class="w-48 shrink-0">
             <h4 class="font-semibold mb-4">Links</h4>
             <ul>
                 <li>
@@ -12,6 +12,9 @@
                 </li>
                 <li>
                     <a href="/admin/posts/create" class="{{ request()->is('admin/posts/create') ? 'text-blue-500' : '' }}">New post</a>
+                </li>
+                <li>
+                    <a href="/admin/posts" class="{{ request()->is('admin/posts') ? 'text-blue-500' : '' }}">Manage posts</a>
                 </li>
             </ul>
         </aside>
